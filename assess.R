76be1adc2f239adcc3d67cedd43bcf1a57a37e5b
Christopher L. Simons <christopherleesimons@gmail.com>
2013-06-15 01:39:03 -0400

    Implemented symmetry assessment.

---
+++ assess.R
@@ -0,0 +1,33 @@
+source("plot_disc.R")
+
+isRowSymmetric <- function(row, alpha) {
+    diff <- 0
+    for (i in 1:floor(length(row) / 2)) {
+        diff <- diff + abs(row[i] - row[length(row) + 1 - i])
+    }
+    return (diff < alpha)
+}
+
+diffSymmetric <- function(row) {
+    diff <- 0
+    for (i in 1:floor(length(row) / 2)) {
+        diff <- diff + abs(row[i] - row[length(row) + 1 - i])
+    }
+    return (diff)
+}
+
+isSymmetric <- function(data, alpha) {
+    z <- build_plot_matrix(data)
+    nrows <- length(z[,1])
+    symmetric <- TRUE
+    for (i in 1:nrows) {
+        row <- z[i,]
+        rowSymmetric <- isRowSymmetric(row, alpha)
+        #print(cat(i, ": row is symmetric (", diffSymmetric(row),
+        #        "<", alpha, ")?: ", rowSymmetric))
+        if (! rowSymmetric) {
+            symmetric <- FALSE
+        }
+    }
+    return (symmetric)
+}
