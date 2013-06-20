c621c7a4d7b427aa7ad15dbf6cc53c2975ac85c4
Christopher L. Simons <christopherleesimons@gmail.com>
2013-06-19 23:19:20 -0400

    Removed unused configuration parameter.

--- src/main.R
+++ src/main.R
@@ -19,7 +19,7 @@ eprint <- function(...) {
 
 args <- commandArgs(trailingOnly = TRUE)
 
-configCorrupt <- function(filename, e) {
+corrupt <- function(filename, e = "") {
     if (length(toString(e)) > 0)
         eprint("Fatal error reading configuration file \"",
                filename, "\":\n\t", toString(e))
@@ -32,11 +32,11 @@ if (length(args) != 1)
     eprint("usage: <script-name> <config-file>")
 
 fileConfig <- args[1]
-tryCatch(source(fileConfig), error=function(e) configCorrupt(fileConfig, e))
-if (!exists("k") || !exists("n")
-        || !exists("generator") || !exists("assessment"))
-    configCorrupt(fileConfig,
-                  "\n\tExpecting {k, n, generator, assessment} to be defined.")
+tryCatch(source(fileConfig), error=function(e) corrupt(fileConfig, e))
+if (!exists("n") || !exists("generator")
+        || !exists("assessment") || !exists("alphas"))
+    corrupt(fileConfig,
+            "Expecting {n, generator, assessment, alphas} to be defined.")
 
 p("Successfully parsed configuration file.")
 
