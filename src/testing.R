3875779db2d7870846ce819989fa14ccecd6895b
Christopher L. Simons <christopherleesimons@gmail.com>
2013-08-22 23:34:55 -0400

    Fixed typographical error in src/testing.R.

--- src/testing.R
+++ src/testing.R
@@ -9,12 +9,14 @@ data <- gen_max_dependence$generate(param.n) # Data set comprising x = y.
 
 # Comparing unrotated and rotated data.
 #
-#   Something is broken here.  In this data set, x = y, yielding a nice
-#   clean plot.  Rotating by pi/2 and pi/3 below give IDENTICAL results,
-#   the results we should get from pi/2, a perfect "90 degree" rotation
-#   of the line.  Using pi/4, however, which should change the plot to a
-#   horizontal line (parallel to the x axis), results in plots scattered
-#   willy nilly.  Perhaps an overflow problem?
+#   Something is broken here.  This data set, x = y, yields a nice clean
+#   scatter plot.  Rotating by pi/2 and pi/3 below give IDENTICAL results
+#   according to the discretization matrix weight matrix output, which I
+#   believe is accurate.  In both cases, we see the results we should get
+#   from pi/2 (a perfect "90 degree" rotation of the line).  Using pi/4,
+#   however, which should change the plot to a horizontal line (parallel
+#   to the x axis), results in plots scattered willy nilly.  Perhaps an
+#   overflow problem?
 #
 #   Once this issue is resolved, we can scale custom_stat and custom_sym
 #   assessments properly and achieve clean 0 < x < 1 metric to compare
