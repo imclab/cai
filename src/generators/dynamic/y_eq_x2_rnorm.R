8253405b6d98e108cf236e3e49472e9024a0a9ac
Christopher L. Simons <christopherleesimons@gmail.com>
2013-10-01 15:06:02 -0400

    Added (y = x^2 + rnorm) dynamic generator.

---
+++ src/generators/dynamic/y_eq_x2_rnorm.R
@@ -0,0 +1,14 @@
+# Christopher L. Simons, 2013
+
+generator <- list(name = paste("y_eq_x2_rnorm::",
+                               VAI__GEN_MOD, sep=""),
+                  dependent = TRUE,
+                  modifiable = TRUE,
+                  generate = function(n) {
+        x <- rnorm(n, 0, 1 * VAI__GEN_MOD)
+        y <- (x^2) * rnorm(n, 0, 1 * VAI__GEN_MOD)
+        return (cbind(x, y))
+    }
+)
+class(generator) <- "generator"
+generators[[length(generators) + 1]] <- generator
