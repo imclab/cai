9b71d417c00fa1f6d440981aaa62d3d3ccc5fd74
Christopher L. Simons <christopherleesimons@gmail.com>
2013-10-13 19:54:14 -0400

    Added ci_pcor.test().

--- src/core/assessments/conditional/pcor.R
+++ src/core/assessments/conditional/pcor.R
@@ -1,10 +1,14 @@
 # Christopher L. Simons, 2013
 
-ci_pcor <- function(x, y, z, cor_method="pearson", alpha=0.05) {
+ci_pcor <- function(x, y, z, cor_method="pearson") {
     resid_xz <- residuals(lm(formula = x ~ z, data = cbind(x, z)))
     resid_yz <- residuals(lm(formula = y ~ z, data = cbind(y, z)))
     return (cor(x = resid_xz,
                 y = resid_yz,
                 use="complete.obs",
                 method=method_cor))
+}
+
+ci_pcor.test <- function(x, y, z, threshold_dep=0.5, cor_method="pearson") {
+    return (abs(ci_pcor(x, y, z, cor_method)) >= threshold_dep)
 }
