462abceb12f6ac39cdb83ade9bd3f1f40a685ffd
Christopher L. Simons <christopherleesimons@gmail.com>
2013-10-14 01:56:31 -0400

    Minor cleanup in breaks.R.

--- src/core/util/breaks.R
+++ src/core/util/breaks.R
@@ -1,6 +1,6 @@
 # Christopher L. Simons, 2013
 
-# Provide break points to establish n discretization bins of equal width.
+# Provide break points to establish n bins of equal width.
 breaks_uniform_width <- function(data, n_bins) {
     vec_min <- min(data)
     vec_max <- max(data)
@@ -15,7 +15,7 @@ breaks_uniform_width <- function(data, n_bins) {
     return (breakpoints)
 }
 
-# Provide break points to establish n discretization bins of equal data points.
+# Provide break points to establish n bins of equal data points.
 breaks_uniform_counts <- function(data, n_bins) {
     bin_count <- length(data) / n_bins
     sorted_data <- sort(data)
@@ -27,24 +27,4 @@ breaks_uniform_counts <- function(data, n_bins) {
         breakpoints[length(breakpoints) + 1] <- next_break
     }
     return (breakpoints)
-}
-
-# Sturges's method (see Margaritis, 2005).
-breaks_dyn_sturges <- function(data) {
-    # TODO: k = 1 + log_2(N) bins of equal width.
-}
-
-# Scott's method (see Margaritis, 2005).
-breaks_dyn_scott <- function(data) {
-    # TODO:
-    # Optimal bin width: h = 3.5sN^(-1/(2+d)),
-    # where s is sample standard deviation,
-    # and d is number of dimensions.
-}
-
-# Freedman and Diaconis's method (see Margaritis, 2005).
-breaks_dyn_freedman_diaconis <- function(data) {
-    # TODO:
-    # Optimal bin width: h = 2(IQ)N^(-1/(2+d)),
-    # where IQ is interquartile range.
 }
