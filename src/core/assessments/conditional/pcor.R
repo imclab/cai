dc6fe4c0678c205e97e4284c58b72a7e9441be1c
Christopher L. Simons <christopherleesimons@gmail.com>
2013-10-13 23:12:56 -0400

    Fixed partial correlation CI test.

--- src/core/assessments/conditional/pcor.R
+++ src/core/assessments/conditional/pcor.R
@@ -1,14 +1,17 @@
 # Christopher L. Simons, 2013
 
-ci_pcor <- function(x, y, z, cor_method="pearson") {
-    resid_xz <- residuals(lm(formula = x ~ z, data = cbind(x, z)))
-    resid_yz <- residuals(lm(formula = y ~ z, data = cbind(y, z)))
-    return (cor(x = resid_xz,
-                y = resid_yz,
-                use="complete.obs",
-                method=method_cor))
+ci_pcor <- function(x, y, z, method_cor="pearson") {
+    data_subset <- data.frame(cbind(x, y, z))
+    names(data_subset) <- c("x", "y", "z")
+
+    resid_xz <- residuals(lm(formula = x ~ z, data = data_subset))
+    resid_yz <- residuals(lm(formula = y ~ z, data = data_subset))
+    return (cor(x      = resid_xz,
+                y      = resid_yz,
+                use    = "complete.obs",
+                method = method_cor))
 }
 
-ci_pcor.test <- function(x, y, z, threshold_dep=0.5, cor_method="pearson") {
+ci_pcor.test <- function(x, y, z, threshold_dep=0.05, cor_method="pearson") {
     return (abs(ci_pcor(x, y, z, cor_method)) >= threshold_dep)
 }
