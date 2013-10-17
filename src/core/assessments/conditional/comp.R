eaa1b5889c3a89cc2345aa6268e4ad747f7049ce
Christopher L. Simons <christopherleesimons@gmail.com>
2013-10-17 12:40:40 -0400

    Fixed bug in comp.R.

--- src/core/assessments/conditional/comp.R
+++ src/core/assessments/conditional/comp.R
@@ -12,7 +12,9 @@ ci_comp <- function(x, y, S, suffStat) {
     y. <- data.[,y]
     S. <- as.matrix(data.[,S])
 
-#p("Called ci_comp(", x, ", ", y, ", ", S, ", <suffStat>[data length = ", ncol(S.), "]).")
+#p("Called ci_comp(", x, ", ", y, ", ", S,
+#  ", |x| = ", length(x.), "; |y| = ", length(y.),
+#  ". [# of conditioning variables = ", ncol(S.), "]).")
     highest <- NULL
     if (ncol(S.) > 0) {
         for (Si in 1:ncol(S.)) {
@@ -25,9 +27,9 @@ ci_comp <- function(x, y, S, suffStat) {
                 xy_subset <- subset(xyz_df,
                                     subset = (  z. >= breaks_z[zb]
                                               & z. <= breaks_z[zb + 1]),
-                                    select = c(x, y))
+                                    select = c(x., y.))
 
-                xy_matrix <- cbind(xy_subset$x, xy_subset$y)
+                xy_matrix <- cbind(xy_subset$x., xy_subset$y.)
 
                 # May not have data for all intervals.
                 if (length(xy_matrix) > 0) {
@@ -42,7 +44,9 @@ ci_comp <- function(x, y, S, suffStat) {
     } else {
         highest <- bivariate_test(cbind(x., y.))
     }
-p("Called ci_comp(", x, ", ", y, ", ", S, ", <suffStat>[data length = ", ncol(S.), "]) --> ",
-    "p::alpha = [", highest, "]::[", thresholds$custom_sc_oppo, "].")
+#p("Called ci_comp(", x, ", ", y, ", ", S,
+#  ", |x| = ", length(x.), "; |y| = ", length(y.),
+#  ". [# of conditioning variables = ", ncol(S.), "]) --> ",
+#  "p::alpha = [", highest, "]::[", thresholds$custom_sc_oppo, "].")
     return (highest)
 }
