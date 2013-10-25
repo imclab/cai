74692c1f0f47925f14efe57f9390aea8e24d2fcf
Christopher L. Simons <christopherleesimons@gmail.com>
2013-10-24 22:01:55 -0400

    Added timing stamps to temperature.continuous.R.

--- src/bin/temperature.continuous.R
+++ src/bin/temperature.continuous.R
@@ -1,6 +1,7 @@
 # Christopher L. Simons, 2013
 
 source("src/core/util/util.R")
+p("Started program at [", date(), "].")
 source("src/conf/properties.R")
 source("src/core/util/init.R")
 source("src/core/learning/support/train.R")
@@ -13,19 +14,18 @@ for (i in 1:length(models))
 results <- list()
 for (i in 1:length(learners)) {
     learner <- learners[[i]]
-    pn("Learning structure using [", sprintf(fmt_s, learner$name), "] ...")
+    pn(date(), ": Learning using [", sprintf(fmt_s, learner$name), "] ...")
     result$struct <- learner$learn(model$data)
     result$learner.name <- learner$name
-    p(" distance from GS = ", shd(model$graph, result$struct), ".")
+    p(" done; SHD = ", shd(model$graph, result$struct), ".")
     results[[length(results) + 1]] <- result
 }
 
 p("Plotting learned structures ...")
-#par(mfrow = c((ceiling((length(results) + 1) / 2)), 2))
 par(mfrow = c(2, (ceiling((length(results) + 1) / 2))))
 plot(model$graph, main = "GS")
 for (i in 1:length(results))
     plot(results[[i]]$struct,
          main = gsub("::", "\n", results[[i]]$learner.name))
 
-p("Done.")
+p("Completed program at [", date(), "] local time.")
