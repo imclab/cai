e5eb6947ae3399989809298559cf58b1dd594612
Christopher L. Simons <christopherleesimons@gmail.com>
2013-10-31 15:54:03 -0400

    Renamed symmetry assessment.

--- src/core/assessments/bivariate/sym_s.R
+++ src/core/assessments/bivariate/sym_s.R
@@ -2,7 +2,7 @@
 
 source("src/core/util/plot_disc.R")
 
-assessment <- list(name = "sym_R", assess = function(data) {
+assessment <- list(name = "sym_s", assess = function(data) {
     row_score <- function(row) {
         diff <- 0
         ncomparisons <- floor(length(row) / 2)
