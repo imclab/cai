f84c6d8486412d6298b1be5d38cb3ec5fc076c01
Christopher L. Simons <christopherleesimons@gmail.com>
2013-11-03 19:16:20 -0500

    Added y=sin(x) generator.

---
+++ src/core/generators/dynamic/y_eq_sinx.R
@@ -0,0 +1,14 @@
+# Christopher L. Simons, 2013
+
+generator <- list(name = paste("y_eq_sinx::",
+                               CAI__GEN_MOD, sep=""),
+                  dependent = TRUE,
+                  modifiable = TRUE,
+                  generate = function(n) {
+        x <- rnorm(n, 0, 1 * CAI__GEN_MOD)
+        y <- sin(x)
+        return (cbind(x, y))
+    }
+)
+class(generator) <- "generator"
+generators[[length(generators) + 1]] <- generator
