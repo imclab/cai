f7ec26ced96cee1f48a0980552562048fefc35f0
Christopher L. Simons <christopherleesimons@gmail.com>
2013-11-12 10:39:48 -0500

    Brought sinusoidal generator in line with Margaritis' F2 generator.

--- src/core/generators/dynamic/y_eq_2sinx.R
+++ src/core/generators/dynamic/y_eq_2sinx.R
@@ -1,12 +1,12 @@
 # Christopher L. Simons, 2013
 
-generator <- list(name = paste("y_eq_sinx::",
+generator <- list(name = paste("y_eq_2sinx::",
                                CAI__GEN_MOD, sep=""),
                   dependent = TRUE,
                   modifiable = TRUE,
                   generate = function(n) {
         x <- rnorm(n, 0, 1 * CAI__GEN_MOD)
-        y <- sin(x) + rnorm(n, 0, 1 * CAI__GEN_MOD)
+        y <- 2*sin(x) + rnorm(n, 0, 1 * CAI__GEN_MOD)
         return (cbind(x, y))
     }
 )
