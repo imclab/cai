6dfc3e94af5f78548a21cbd4b72263322c47e5e5
Christopher L. Simons <christopherleesimons@gmail.com>
2013-11-12 10:38:46 -0500

    Added Margaritis F4 generator.

---
+++ src/core/generators/dynamic/y_eq_f4.R
@@ -0,0 +1,14 @@
+# Christopher L. Simons, 2013
+
+generator <- list(name = paste("y_eq_1/((x/5)+1)::",
+                               CAI__GEN_MOD, sep=""),
+                  dependent = TRUE,
+                  modifiable = TRUE,
+                  generate = function(n) {
+        x <- rnorm(n, 0, 1 * CAI__GEN_MOD)
+        y <- 1/((x/5) + 1) + rnorm(n, 0, 1 * CAI__GEN_MOD)
+        return (cbind(x, y))
+    }
+)
+class(generator) <- "generator"
+generators[[length(generators) + 1]] <- generator
