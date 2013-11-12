00ed6159d4ad5aadd608f2edddd6e0a4a55681e7
Christopher L. Simons <christopherleesimons@gmail.com>
2013-11-12 10:28:55 -0500

    Added f(x) = ln(|x|) generator.

---
+++ src/core/generators/dynamic/y_eq_ln_x.R
@@ -0,0 +1,14 @@
+# Christopher L. Simons, 2013
+
+generator <- list(name = paste("y_eq_ln_|x|+rnorm::",
+                               CAI__GEN_MOD, sep=""),
+                  dependent = TRUE,
+                  modifiable = TRUE,
+                  generate = function(n) {
+        x <- rnorm(n, 0, 1 * CAI__GEN_MOD)
+        y <- log(abs(x)) + rnorm(n, 0, 1 * CAI__GEN_MOD)
+        return (cbind(x, y))
+    }
+)
+class(generator) <- "generator"
+generators[[length(generators) + 1]] <- generator
