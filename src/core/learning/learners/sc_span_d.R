d323bbc91dbf9d9028a8e799c5012a2b6bb08e89
Christopher L. Simons <christopherleesimons@gmail.com>
2013-11-19 22:35:44 -0500

    Updated SC learner per recent developments.

--- src/core/learning/learners/sc_span_d.R
+++ src/core/learning/learners/sc_span_d.R
@@ -1,14 +1,14 @@
 # Christopher L. Simons, 2013
 
-learner <- list(name  = "sc_dev_d",
+learner <- list(name  = "sc_span_d",
                 learn = function(data.) {
                     result <- pc(suffStat
                                    = list(data = data.,
                                           bivariate_test =
-                                            assessments$sc_dev_d),
+                                            assessments$sc_span_d),
                                  indepTest = ci_comp,
                                  p         = ncol(data.),
-                                 alpha     = thresholds$sc_dev_d)
+                                 alpha     = thresholds$sc_span_d)
                     nodes(result@graph) <- names(data.)
                     return (result)
                 })
