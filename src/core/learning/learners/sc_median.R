52c7a308b3dac339fa21568dff8ab5da984fa864
Christopher L. Simons <christopherleesimons@gmail.com>
2013-11-26 00:09:47 -0500

    Reorganized leaners based on recent developments.

--- src/core/learning/learners/sc_median.R
+++ src/core/learning/learners/sc_median.R
@@ -1,14 +1,14 @@
 # Christopher L. Simons, 2013
 
-learner <- list(name  = "sc_span_d",
+learner <- list(name  = "sc_median",
                 learn = function(data.) {
                     result <- pc(suffStat
                                    = list(data = data.,
                                           bivariate_test =
-                                            assessments$sc_span_d),
+                                            assessments$sc_median),
                                  indepTest = ci_comp,
                                  p         = ncol(data.),
-                                 alpha     = thresholds$sc_span_d)
+                                 alpha     = thresholds$sc_median)
                     nodes(result@graph) <- names(data.)
                     return (result)
                 })
