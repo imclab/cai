603881653ca4429db8a2de4284984502bfa20679
Christopher L. Simons <christopherleesimons@gmail.com>
2013-11-26 19:52:59 -0500

    Updating default parameter configuration.

--- src/conf/properties.R
+++ src/conf/properties.R
@@ -1,6 +1,6 @@
-param.verbose = FALSE
+param.verbose = TRUE
 
-training.n    = 5000
+training.n    = 2000
 testing.n     = 20000 # n >= 30 @ / 10 x 10 x 10 = 1000
 fmt_s         = "%19s"
 break.fixed.n = 10
@@ -8,8 +8,8 @@ break_method  = "fixed" # alternative: "sturges"
 
 # Generator "sigma" modifiers.
 CAI__GEN_MOD_DEFAULT = 1
-#CAI__GEN_MODS        = c(0.01, 0.1, 1, 10)
-CAI__GEN_MODS        = c(1)
+CAI__GEN_MODS        = c(0.01, 0.1, 1, 10, 100)
+#CAI__GEN_MODS        = c(1)
 
 AUTOLOAD_DIRS = c(
     "src/core/generators",
