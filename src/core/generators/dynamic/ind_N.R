30f8c714e0773e973ea0c2bcff6665232235e58a
Christopher L. Simons <christopherleesimons@gmail.com>
2013-12-02 10:50:25 -0500

    Reorganized independent generators.

--- src/core/generators/dynamic/ind_N.R
+++ src/core/generators/dynamic/ind_N.R
@@ -1,11 +1,11 @@
 # Christopher L. Simons, 2013
 
-generator <- list(name = paste("ind_UxN::",
+generator <- list(name = paste("ind_N::",
                                CAI__GEN_MOD, sep=""),
                   dependent = FALSE,
                   modifiable = TRUE,
                   generate = function(n) {
-        x <- runif(n, 0, 1 * CAI__GEN_MOD)
+        x <- rnorm(n, 0, 1 * CAI__GEN_MOD)
         y <- 0 + rnorm(n, 0, 1 * CAI__GEN_MOD)
         return (cbind(x, y))
     }
