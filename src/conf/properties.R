aa313a70ce98dd8de8545560fdc9d73cd66a656c
Christopher L. Simons <christopherleesimons@gmail.com>
2013-12-05 12:16:35 -0500

    Updated properties per recent preferences.

--- src/conf/properties.R
+++ src/conf/properties.R
@@ -1,10 +1,11 @@
 param.verbose = TRUE
 
 training.n    = 2000
-testing.n     = 20000 # n >= 30 @ / 10 x 10 x 10 = 1000
+testing.n     = 20000
 fmt_s         = "%19s"
-break.fixed.n = 10
-break_method  = "fixed" # alternative: "sturges"
+break_method  = "sturges"
+#break_method  = "fixed"
+#break.fixed.n = 20
 
 # Generator "sigma" modifiers.
 CAI__GEN_MOD_DEFAULT = 1
