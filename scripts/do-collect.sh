da34a60339563f0b8c76fc7c521d4803c93a4d78
Christopher L. Simons <christopherleesimons@gmail.com>
2013-12-05 12:47:45 -0500

    Many robustness improvements in do-collect.sh script.

--- scripts/do-collect.sh
+++ scripts/do-collect.sh
@@ -11,15 +11,42 @@ fi
 model=$1
 n=$2
 
-echo "Running $n iterations of model $model ..."
+if [ ! $n -gt 0 ]
+then
+    echo "<run-count> must be a positive integer."
+    exit 1
+fi
+
+if [ ! -e "./src/bin/${model}.R" ]
+then
+    echo "Model script '$model' not found."
+    exit 1
+fi
+
+if [ ! -e "./src/bin" ]
+then
+    echo "Must be run from project root directory."
+    exit 1
+fi
+
+mkdir -p results
+
+echo -n "Running $n iterations of model $model ... "
 
 i=0
 while [ $i -lt $n ]
 do
     filebase="results/`date +%Y-%m-%d-%H%M`-$model"
-    Rscript src/bin/$model.R > ${filebase}.txt
+    Rscript src/bin/$model.R > ${filebase}.log 2>&1
+
+    if [ $? -ne 0 ]
+    then
+        echo "aborting batch collection (Rscript completed in error)."
+        exit 1
+    fi
+
     mv Rplots.pdf ${filebase}.pdf
     i=$[$i+1]
 done
 
-echo "Completed batch run."
+echo "done."
