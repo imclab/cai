a9fa5fbe988056913152fe435469532fac068da1
Christopher L. Simons <christopherleesimons@gmail.com>
2013-12-11 02:13:58 -0500

    Added sqrt(x) generator.

---
+++ src/core/generators/dynamic/sqrtx.R
@@ -0,0 +1,14 @@
+# Christopher L. Simons, 2013
+
+generator <- list(name = paste("\\sqrt{x}::",
+                               CAI__GEN_MOD, sep=""),
+                  dependent = TRUE,
+                  modifiable = TRUE,
+                  generate = function(n) {
+        x <- rnorm(n, 0, 1 * CAI__GEN_MOD)
+        y <- sqrt(x) + rnorm(n, 0, 1 * CAI__GEN_MOD)
+        return (cbind(x, y))
+    }
+)
+class(generator) <- "generator"
+generators[[length(generators) + 1]] <- generator
