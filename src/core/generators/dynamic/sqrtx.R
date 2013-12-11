8bd4573308ac68015925bb4c4a75e2e3907b4330
Christopher L. Simons <christopherleesimons@gmail.com>
2013-12-11 02:47:11 -0500

    Protecting against negative arguments to sqrt() in sqrtx generator.

--- src/core/generators/dynamic/sqrtx.R
+++ src/core/generators/dynamic/sqrtx.R
@@ -1,12 +1,12 @@
 # Christopher L. Simons, 2013
 
-generator <- list(name = paste("\\sqrt{x}::",
+generator <- list(name = paste("\\sqrt{|x|}::",
                                CAI__GEN_MOD, sep=""),
                   dependent = TRUE,
                   modifiable = TRUE,
                   generate = function(n) {
         x <- rnorm(n, 0, 1 * CAI__GEN_MOD)
-        y <- sqrt(x) + rnorm(n, 0, 1 * CAI__GEN_MOD)
+        y <- sqrt(abs(x)) + rnorm(n, 0, 1 * CAI__GEN_MOD)
         return (cbind(x, y))
     }
 )
