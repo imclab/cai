b5db33264a5fa85e7d8bb7f5ddc688032da8f196
Christopher L. Simons <christopherleesimons@gmail.com>
2013-12-11 02:12:33 -0500

    Replaced 2sinx generator with sinx generator (consistency).

--- src/core/generators/dynamic/sinx.R
+++ src/core/generators/dynamic/sinx.R
@@ -1,12 +1,12 @@
 # Christopher L. Simons, 2013
 
-generator <- list(name = paste("2 sin(x)::",
+generator <- list(name = paste("sin(x)::",
                                CAI__GEN_MOD, sep=""),
                   dependent = TRUE,
                   modifiable = TRUE,
                   generate = function(n) {
         x <- rnorm(n, 0, 1 * CAI__GEN_MOD)
-        y <- 2*sin(x) + rnorm(n, 0, 1 * CAI__GEN_MOD)
+        y <- sin(x) + rnorm(n, 0, 1 * CAI__GEN_MOD)
         return (cbind(x, y))
     }
 )
