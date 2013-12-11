7c3a9105a6743ef46fda965ea43725982aded5fc
Christopher L. Simons <christopherleesimons@gmail.com>
2013-12-11 02:48:01 -0500

    Fixed broken assessment name reference in sc_median learner.

--- src/core/learning/learners/sc_median.R
+++ src/core/learning/learners/sc_median.R
@@ -1,14 +1,14 @@
 # Christopher L. Simons, 2013
 
-learner <- list(name  = "sc_median",
+learner <- list(name  = "SC_median",
                 learn = function(data.) {
                     result <- pc(suffStat
                                    = list(data = data.,
                                           bivariate_test =
-                                            assessments$sc_median),
+                                            assessments[["SC_{\\widetilde{x}}"]]),
                                  indepTest = ci_comp,
                                  p         = ncol(data.),
-                                 alpha     = thresholds$sc_median)
+                                 alpha     = thresholds[["SC_{\\widetilde{x}}"]])
                     nodes(result@graph) <- names(data.)
                     return (result)
                 })
