e7f2de404d8f84ce10c81a0a5802d9c1218c04ef
Christopher L. Simons <christopherleesimons@gmail.com>
2014-01-14 12:38:24 -0500

    Reorganized generators tree.

--- src/core/generators/__AUTOLOAD.R
+++ src/core/generators/__AUTOLOAD.R
@@ -1,11 +1,12 @@
 generators <- list()
 
-for (dirname in c("src/core/generators/static"))
-    if (length(dirname) > 0)
-        for (filename in list.files(path = dirname, pattern = ".+\\.R"))
-            source(paste(dirname, "/", filename, sep = ""))
+%for (dirname in c("src/core/generators/static"))
+%    if (length(dirname) > 0)
+%        for (filename in list.files(path = dirname, pattern = ".+\\.R"))
+%            source(paste(dirname, "/", filename, sep = ""))
 
-for (dirname in c("src/core/generators/dynamic")) {
+%for (dirname in c("src/core/generators/dynamic")) {
+for (dirname in c("src/core/generators")) {
     if (length(dirname) > 0) {
         for (filename in list.files(path = dirname, pattern = ".+\\.R")) {
             for (i in 1:length(CAI__GEN_MODS)) {
