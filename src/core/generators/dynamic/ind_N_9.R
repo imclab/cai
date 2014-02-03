a1522accc7db1bcb6a8a453e61691587e7565464
Christopher L. Simons <christopherleesimons@gmail.com>
2014-02-03 14:39:41 -0500

    Adding six more normally-distributed independent generators.

---
+++ src/core/generators/dynamic/ind_N_9.R
@@ -0,0 +1,14 @@
+# Christopher L. Simons, 2013
+
+generator <- list(name = paste("\\noise_9::",
+                               CAI__GEN_MOD, sep=""),
+                  dependent = FALSE,
+                  modifiable = TRUE,
+                  generate = function(n) {
+        x <- rnorm(n, 0, 1 * CAI__GEN_MOD)
+        y <- 0 + rnorm(n, 0, 1 * CAI__GEN_MOD)
+        return (cbind(x, y))
+    }
+)
+class(generator) <- "generator"
+generators[[length(generators) + 1]] <- generator
