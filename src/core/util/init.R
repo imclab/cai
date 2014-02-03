8eff711d493b740957f04675222542ad9d0fac73
Christopher L. Simons <christopherleesimons@gmail.com>
2014-02-03 15:07:29 -0500

    Improvement in bivariate-comp output.

--- src/core/util/init.R
+++ src/core/util/init.R
@@ -23,3 +23,7 @@ for (dirname in AUTOLOAD_DIRS)
     if (length(dirname) > 0)
         for (filename in list.files(path = dirname, pattern = ".+\\.R"))
             source(paste(dirname, "/", filename, sep = ""))
+
+if (length(CAI__GEN_MODS) == 1)
+    for (generator in generators)
+        generator$name <- gsub("::([^ ]+)", "", generator$name)
