ca3b67ef5ab4e6ea8f051b7a47630c9eae721a86
Christopher L. Simons <christopherleesimons@gmail.com>
2014-02-05 02:21:32 -0500

    Improvements in bivariate-comp script.

--- scripts/bivariate-comp.sh
+++ scripts/bivariate-comp.sh
@@ -2,6 +2,8 @@
 
 echo -n "Generating bivariate-comparison table ... "
 
+[ -f bivariate-comp.csv ] && rm bivariate-comp.csv
+
 Rscript src/bin/bivariate-comp.R > /dev/null 2>&1
 
 if [ $? -ne 0 ]
@@ -9,5 +11,7 @@ then
     echo "aborting (Rscript completed in error)."
     exit 1
 fi
+
+sed -i "s/::1//g" bivariate-comp.csv
 
 echo "done."
