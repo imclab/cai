05632379701af513efb45068b09b9df589f11743
Christopher L. Simons <christopherleesimons@gmail.com>
2014-02-05 02:28:34 -0500

    Added U(0,1) generator.

---
+++ src/core/generators/dynamic/51_unif.R
@@ -0,0 +1,14 @@
+# Christopher L. Simons, 2013
+
+generator <- list(name = paste("\\noise::",
+                               CAI__GEN_MOD, sep=""),
+                  dependent = FALSE,
+                  modifiable = TRUE,
+                  generate = function(n) {
+        x <- rnorm(n, 0, 1 * CAI__GEN_MOD)
+        y <- runif(n, 0, 1 * CAI__GEN_MOD)
+        return (cbind(x, y))
+    }
+)
+class(generator) <- "generator"
+generators[[length(generators) + 1]] <- generator
