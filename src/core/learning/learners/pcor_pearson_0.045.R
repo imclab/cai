7b11530dff1160a5e14d4c2ffdc0146ca61cb9ab
Christopher L. Simons <christopherleesimons@gmail.com>
2014-02-06 13:02:36 -0500

    Retired Pearson/a=0.25 PCor learner and created a=0.045 learner.

---
+++ src/core/learning/learners/pcor_pearson_0.045.R
@@ -0,0 +1,15 @@
+# Christopher L. Simons, 2013
+
+learner <- list(name  = "pcor::pearson::0.045",
+                learn = function(data.) {
+                    result <- pc(suffStat = list(data = data.,
+                                                 method_cor = "pearson"),
+                                 indepTest = ci_pcor,
+                                 p         = ncol(data.),
+                                 alpha     = 0.045)
+                    nodes(result@graph) <- names(data.)
+                    return (result)
+                })
+
+class(learner) <- "learner"
+learners[[length(learners) + 1]] <- learner
