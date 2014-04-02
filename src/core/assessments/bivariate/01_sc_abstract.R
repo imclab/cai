cad82e12667b10d5e149478f05869220affe72f1
Christopher L. Simons <christopherleesimons@gmail.com>
2014-04-02 11:48:54 -0400

    Added pre-sorting optimizations to 01_sc_abstract.R (significant performance improvement).

--- src/core/assessments/bivariate/01_sc_abstract.R
+++ src/core/assessments/bivariate/01_sc_abstract.R
@@ -6,21 +6,29 @@ createSCAssessment <- function(stat.tex.name, fn.name)
     {
         axisScore <- function(data)
         {
-            breaks.x <- breaksUniformWidth(data[,1], binCount(nrow(data)))
+            data <- data[order(data[,1]),] # Sort by first column.
+            breaks.x <- breaksUniformWidth(data[,1],
+                                           binCount(nrow(data)),
+                                           preSorted=TRUE)
 
             # Walk along x-axis creating partitions.
 
+            all.values <- c()
             partition.stats <- c()
             for (xb in 1:(length(breaks.x) - 1)) {
                 partition.values <- c()
 
-                for (i in 1:length(data[,1])) {
+                for (i in (1 + length(all.values)):length(data[,1])) {
                     xi <- data[i,][1]
                     yi <- data[i,][2]
 
                     if (xi >= breaks.x[xb] && xi <= breaks.x[xb + 1])
                         partition.values <- append(partition.values, yi)
+                    else
+                        break # Sorted, so once we get here we're done.
                 }
+
+                all.values <- append(all.values, partition.values)
 
                 partition.stats <- append(partition.stats,
                                     if (length(partition.values) > 0)
