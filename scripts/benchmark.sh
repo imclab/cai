f7080a0e4d1503e633b25fc7f3a760701a23177c
Christopher L. Simons <christopherleesimons@gmail.com>
2014-05-07 23:25:31 -0400

    Added CSV adjustment logic to benchmark script.

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if ! `hash perl >/dev/null 2>&1`
+then
+    echo "This script depends on perl, which is not on the PATH."
+    exit 1
+fi
+
 echo -n "Running benchmark ... "
 
 Rscript src/bin/benchmark.R > /dev/null 2>&1
@@ -9,5 +15,18 @@ then
     echo "aborting (Rscript completed in error)."
     exit 1
 fi
+
+echo "done."
+
+echo -n "Adjusting benchmark output ... "
+
+perl -pi -e '
+    s/^"\d*",//;
+    s/"p-values"/"gold-p","mode-p","pcor-p"/;
+    s/gold=//;
+    s/mode=//;
+    s/pcor=//;
+    s/;/","/g;
+    ' benchmark.csv
 
 echo "done."
