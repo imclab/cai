96f1298adf166f79913a2b04974d34130402e4e4
Christopher L. Simons <christopherleesimons@gmail.com>
2014-05-07 23:25:17 -0400

    Added CSV adjustment logic to ROC data generation script.

--- scripts/roc.sh
+++ scripts/roc.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if ! `hash perl >/dev/null 2>&1`
+then
+    echo "This script depends on perl, which is not on the PATH."
+    exit 1
+fi
+
 echo -n "Generating ROC data ... "
 
 Rscript src/bin/roc.R > /dev/null 2>&1
@@ -9,5 +15,13 @@ then
     echo "aborting (Rscript completed in error)."
     exit 1
 fi
+
+echo "done."
+
+echo -n "Adjusting ROC data ... "
+
+perl -pi -e '
+    s/^"\d*",//;
+    ' roc.csv
 
 echo "done."
